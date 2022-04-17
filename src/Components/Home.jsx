d989d5e4dd541f3a4f1a79dcc8fdcb5b7c9a30be
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-04-17 20:47:16 +0530

    material ui applied on home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,16 @@
 import axios from "axios";
+import * as React from "react";
 import { useEffect, useState } from "react";
 import { useNavigate, Navigate } from "react-router-dom";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 export const Home = () => {
   const local = JSON.parse(localStorage.getItem("userdata")) || false;
@@ -32,37 +42,40 @@ export const Home = () => {
       {status ? (
         <>
           {" "}
-          <h1>Home Page</h1>
-          <table>
-            <thead>
-              <tr>
-                <td>FlatNo</td>
-                <td>FlatType</td>
-                <td>Block</td>
-                <td>Residents</td>
-                <td>Details</td>
-              </tr>
-            </thead>
-            <tbody>
-              {flats.map((e) => {
-                return (
-                  <tr>
-                    <td>{e.flatnumber}</td>
-                    <td>{e.type}</td>
-                    <td>{e.block}</td>
-                    <td>{"no of residents"}</td>
-                    <td
-                      onClick={() => {
-                        navigate(`/flats/${e._id}`);
-                      }}
-                    >
-                      {"Details"}
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          <h2>Home Page</h2>
+          <TableContainer component={Paper} style={{width:"80%", margin:"auto"}} >
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow style={{ backgroundColor: "teal" }}>
+                  <TableCell align="center">Flat.No</TableCell>
+                  <TableCell align="center">Flat Type</TableCell>
+                  <TableCell align="center">Block</TableCell>
+                  <TableCell align="center">Residents</TableCell>
+                  <TableCell align="center">Details</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {flats.map((e) => {
+                  return (
+                    <TableRow>
+                      <TableCell align="center">{e.flatnumber}</TableCell>
+                      <TableCell align="center">{e.type}</TableCell>
+                      <TableCell align="center">{e.block}</TableCell>
+                      <TableCell align="center">{"no of residents"}</TableCell>
+                      <TableCell
+                        onClick={() => {
+                          navigate(`/flats/${e._id}`);
+                        }}
+                        align="center"
+                      >
+                        {"Details"}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </>
       ) : (
         <Navigate to={"/login"} />
